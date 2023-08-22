a5ace068b41980596b0e71f23b60929b0c945e84
NicoBrites <nicolas.britesv@gmail.com>
2023-08-22 18:39:49 -0300

    Agregado de Test Unitarios.

---
+++ Practica2Tests1/Entidades/IntegerExtendedTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Practica2.Entidades;
+using Practica2.Extensiones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2.Extensiones.Tests
+{
+    [TestClass()]
+    public class IntegerExtendedTests
+    {
+        [TestMethod()]
+        public void DividirPorCeroTest()
+        {
+            // Arrange
+            int num = 70;
+            int esperado = -1;
+            int retorno;
+
+            // Act
+            retorno = num.DividirPorCero();
+
+            // Assert
+            Assert.AreEqual(esperado, retorno);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void LanzarExcepcionTest()
+        {
+            // Arrange
+
+            // Act
+            Logic.LanzarExcepcion();
+
+            // Assert
+        }
+    }
+}
