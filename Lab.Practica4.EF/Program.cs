41cd280f927415343afc6ee7f8e348ea76883d23
NicoBrites <nicolas.britesv@gmail.com>
2023-09-03 18:13:20 -0300

    Prueba git.

---
+++ Lab.Practica4.EF/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.Practica4.EF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
