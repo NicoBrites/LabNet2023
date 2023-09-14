8ef06e13c5b6c33b3048a5e0fec495cfef7ffedf
NicoBrites <nicolas.britesv@gmail.com>
2023-09-13 23:56:38 -0300

    Proligidad. Borrado de usings sin uso.

--- Lab.Practica3.EF.Logic/BaseLogic.cs
+++ Lab.Practica3.EF.Logic/BaseLogic.cs
@@ -1,9 +1,4 @@
 ﻿using Lab.Practica3.EF.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab.Practica3.EF.Logic
 {
